dd4b95c0a48a077e522f5b418bfe5f67d6b2d0b1
Jordan Lewis (UNSW z5165128) <jordan.lewis@student.unsw.edu.au>
2017-03-12 23:47:38 +1100

    Remove prompt warning from template

--- template.c
+++ template.c
@@ -4,6 +4,15 @@
 
 #include <stdio.h>
 
+int get_input(char *prompt, int *x) {
+	printf("%s", prompt);
+	if (scanf("%d", x) == 0) {
+		printf("Value entered was not an integer.\n");
+		return 0;
+	}
+	return 1;
+}
+
 int main () {
 
 	return 0;

21a70720fbc3131f45a19eb559bf1270b233016c
Jordan Lewis (UNSW z5165128) <jordan.lewis@student.unsw.edu.au>
2017-03-13 10:30:29 +1100

    Complete Lab03 challenge 1, one_loop_sqare

---
+++ lab03/one_loop_square.c
@@ -0,0 +1,38 @@
+// Jordan Lewis (jordan.lewis@student.unsw.edu.au)
+// March 12 2017
+// Reads an integer and prints a square of asterisks that size
+
+#include <stdio.h>
+#include <math.h>
+
+int get_input(char *prompt, int *x) {
+	printf("%s", prompt);
+	if (scanf("%d", x) == 0) {
+		printf("Value entered was not an integer.\n");
+		return 0;
+	}
+	return 1;
+}
+
+#define SQ (int)pow((double)size, 2)
+#define DIVABLE (counter+1) % size == 0
+
+int main () {
+	
+	int size;
+
+	if (!get_input("Square size: ", &size)) {
+	       return 1;
+	}	       
+	
+	int counter = 0;
+	while (counter < SQ) {
+		printf("*");
+		if (DIVABLE) {
+			printf("\n");
+		}
+		counter++;
+	}
+
+	return 0;
+}

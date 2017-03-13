3d6121f6f1860110bbaecad9c70c9624221cdfa5
Jordan Lewis (UNSW z5165128) <jordan.lewis@student.unsw.edu.au>
2017-03-13 12:10:34 +1100

    Complete Challenge 1 for Lab03, all tests passed

--- lab03/order3_challenge1.c
+++ lab03/order3_challenge1.c
@@ -5,25 +5,50 @@
 #include <stdio.h>
 
 #define PROMPT "Enter integer: "
+#define DEBUGGING 0
+
+void prog(int x, int y, int z) {
+	// Making use of the fact that booleans in C are simply 0/1 integers to i
+	int arr[4] = {};
+	arr[(x>y) + (x>z)] = x;
+	arr[(y>x) + (y>z)] = y;
+	arr[(z>x) + (z>y)] = z;
+	if (DEBUGGING) { 
+		printf("The integers in order are: %d %d %d\n", arr[0], arr[1], arr[2]);
+	}
+	arr[(arr[0] != 0) * 3] = arr[1];
+	arr[((arr[1] != 0) * 3) + (arr[1] == 0)] = arr[0];
+	arr[((arr[2] != 0) * 3) + (arr[2] == 0)*2] = arr[1];
+	
+	printf("The integers in order are: %d %d %d\n", arr[0], arr[1], arr[2]);
+	if (DEBUGGING) {
+		printf("-----\n");
+	}
+}
 
 int main () {
 	
 	int x,y,z;
-	printf(PROMPT);
-	scanf("%d", &x);
-	printf(PROMPT);
-	scanf("%d", &y);
-	printf(PROMPT);
-	scanf("%d", &z);
+	if (!DEBUGGING) {
+		printf(PROMPT);
+		scanf("%d", &x);
+		printf(PROMPT);
+		scanf("%d", &y);
+		printf(PROMPT);
+		scanf("%d", &z);
+		
+		prog(x,y,z);
+	} else {
+		int test[4][3] = {
+			{1,1,2},
+			{1,2,1},
+			{2,1,1},
+			{1,1,1}
+		};
 
-	// Making use of the fact that booleans in C are simply 0/1 integers to insert the right numbers into the correct positions in an array, then to read them back in order.	
-	int arr[3] = {};
-	arr[(x>=y) + (x>=z)] = x;
-	arr[(y>=x) + (y>=z)] = y;
-	arr[(z>=x) + (z>=y)] = z;
-
-
-	printf("The integers in order are: %d %d %d\n", arr[0], arr[1], arr[2]);
-
+		for (int i = 0; i < 4; i++) {
+			prog(test[i][0], test[i][1], test[i][2]);
+		}
+	}
 	return 0;
 }

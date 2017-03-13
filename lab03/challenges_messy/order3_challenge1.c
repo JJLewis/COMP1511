a7f275610ceb23883598ca9999426e1e12e02758
Jordan Lewis (UNSW z5165128) <jordan.lewis@student.unsw.edu.au>
2017-03-13 12:58:49 +1100

    Clean up Lab03 challenges

--- lab03/challenges_messy/order3_challenge1.c
+++ lab03/challenges_messy/order3_challenge1.c
@@ -7,21 +7,15 @@
 #define PROMPT "Enter integer: "
 #define DEBUGGING 0
 
-int main () {
-	
-	int x,y,z;
-	printf(PROMPT);
-	scanf("%d", &x);
-	printf(PROMPT);
-	scanf("%d", &y);
-	printf(PROMPT);
-	scanf("%d", &z);
-		
+void prog(int x, int y, int z) {
 	// Making use of the fact that booleans in C are simply 0/1 integers to i
 	int arr[4] = {};
 	arr[(x>y) + (x>z)] = x;
 	arr[(y>x) + (y>z)] = y;
 	arr[(z>x) + (z>y)] = z;
+	if (DEBUGGING) { 
+		printf("The integers in order are: %d %d %d\n", arr[0], arr[1], arr[2]);
+	}
 
 	// Correct problems with duplicate and triple entries
 	arr[(arr[0] != 0) * 3] = arr[1];
@@ -29,6 +23,36 @@ int main () {
 	arr[((arr[2] != 0) * 3) + (arr[2] == 0)*2] = arr[1];
 	
 	printf("The integers in order are: %d %d %d\n", arr[0], arr[1], arr[2]);
+	if (DEBUGGING) {
+		printf("-----\n");
+	}
+}
+
+int main () {
 	
+	int x,y,z;
+	if (!DEBUGGING) {
+		printf(PROMPT);
+		scanf("%d", &x);
+		printf(PROMPT);
+		scanf("%d", &y);
+		printf(PROMPT);
+		scanf("%d", &z);
+		
+		prog(x,y,z);
+	} else {
+
+		// Code to test my correction for duplicate and triple entries
+		int test[4][3] = {
+			{1,1,2},
+			{1,2,1},
+			{2,1,1},
+			{1,1,1}
+		};
+
+		for (int i = 0; i < 4; i++) {
+			prog(test[i][0], test[i][1], test[i][2]);
+		}
+	}
 	return 0;
 }

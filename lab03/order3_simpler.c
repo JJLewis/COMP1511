4ed0a61293cd39523eae6ab9572f0a05496279dd
Jordan Lewis <jordan_j_lewis@icloud.com>
2017-03-13 21:23:21 +1100

    Add comments to lab03 order3 simpler, for easier understanding

--- lab03/order3_simpler.c
+++ lab03/order3_simpler.c
@@ -5,7 +5,9 @@
 #include <stdio.h>
 
 int main () {
+
 	int x,y,z;
+
 	printf("Enter Integer: ");
 	scanf("%d", &x);
 	printf("Enter Integer: ");
@@ -15,20 +17,26 @@ int main () {
 
 	int o1,o2,o3;
 
+	// If X is the largest
 	if (x > y && x > z) {
 		o1 = x;
+		// Now if Y is greater than Z, then Y must be in the middle and Z be the smallest
 		if (y > z) {
 			o2 = y;
 			o3 = z;
 		}
+		// Refer to the above, but now test if Y is the largest
 	} else if (y > x && y > z) {
 		o1 = y;
+		// Again, find the 2nd & 3rd numbers like the above comment
 		if (x > z) {
 			o2 = x;
 			o3 = z;
 		}
+		// Same again but for Z
 	} else if (z > x && z > y) {
 		o1 = z;
+		// Again, find the 2nd & 3rd numbers like the above comment
 		if (x > y) {
 			o2 = x;
 			o3 = y;

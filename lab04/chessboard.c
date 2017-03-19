5d15353f06a0c51b4d3388504a69a5cceda21f38
Jordan Lewis (UNSW z5165128) <jordan.lewis@student.unsw.edu.au>
2017-03-19 21:49:41 +1100

    Start Lab04 chessboard.c

---
+++ lab04/chessboard.c
@@ -0,0 +1,34 @@
+// Jordan Lewis (jordan.lewis@student.unsw.edu.au)
+// Mar 19 2017
+// Reads an integer n from standard input. and prints an nxn pattern of asterisks and dashes in the shape of a chessboard.
+
+#include <stdio.h>
+
+int get_input(const char *prompt, int *x) {
+	printf("%s", prompt);
+	if (scanf("%d", x) == 0) {
+		printf("Value entered was not an integer.\n");
+		return 0;
+	}
+	return 1;
+}
+
+#define ODD_ROW (vert % 2 == 0)
+#define ODD_COL (hor % 2 == 0)
+
+int main () {
+	int input;
+	if (!get_input(": ", &input)) {
+	       return 1;
+	}
+
+	int vert = 0;
+	while (vert < input) {
+		int hor = 0;
+		while (hor < input) {
+			if (ODD_ROW) {
+				if (ODD_COL) {
+										
+
+	return 0;
+}

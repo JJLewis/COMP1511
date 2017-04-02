32ae7f20b178918c72fb1a5c84af15e0ba961311
Jordan Lewis (UNSW z5165128) <jordan.lewis@student.unsw.edu.au>
2017-04-03 00:34:37 +1000

    Lab05 io, needs fixing

--- lab05/farnarkle_io.c
+++ lab05/farnarkle_io.c
@@ -5,27 +5,18 @@
 #include <stdio.h>
 #include "farnarkle.h"
 
-int get_input(const char *prompt, int *x) {
-	printf("%s", prompt);
-	if (scanf("%d", x) == 0) {
-		printf("Value entered was not an integer.\n");
-		return 0;
-	}
-	return 1;
-}
-
 // read N_TILES tiles into array tiles
 // return 1 if successful, 0 otherwise
 int read_tiles(int tiles[N_TILES]) {
 	int counter = 0;
 	while (counter < N_TILES) {
 		int input;
-		if(get_input("", &input)) {
+		if(scanf("%d", &input) == 0) {
+			return 0;
+		} else {
 			if (input > 0 && input <= MAX_TILE) {
 				tiles[counter] = input;
 			}
-		} else {
-			return 0;
 		}
 		counter++;
 	}

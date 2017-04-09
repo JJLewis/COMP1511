7e3a3ba2fe125dcd8372e4fed5cc179ab36f7bdf
Jordan Lewis (UNSW z5165128) <jordan.lewis@student.unsw.edu.au>
2017-04-09 16:14:58 +1000

    Create a simple caesar script to bruteforce crack, by showing all options

---
+++ lab06/caesar_bf.c
@@ -0,0 +1,88 @@
+// Jordan Lewis (jordan.lewis@student.unsw.edu.au)
+// 7 April 2017
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define A 'A'//65
+#define Z 'Z'//90
+#define a 'a'//97
+#define z 'z'//122
+
+int isUpper(int c) {
+	return A <= c && c <= Z;
+}
+
+int isLower(int c) {
+	return a <= c && c <= z;
+}
+
+int isAlphabet(int c) {
+	return isUpper(c) || isLower(c);	
+}
+
+int isOutLower(int c, int nc) {
+	int limit = isUpper(c) ? A:a;	
+	return nc < limit;
+}
+
+int isOutUpper(int c, int nc) {
+	int limit = isUpper(c) ? Z:z;
+	return nc > limit;
+}
+
+int isOut(int c, int nc) {	
+	return isOutUpper(c, nc) || isOutLower(c, nc);
+}	
+
+int fixOutUpper(int c, int upper, int lower, int shift) {
+	return (lower - (upper - (c + shift))) - 1;
+}
+
+int fixOutLower(int c, int upper, int lower, int shift) {
+	return (upper - (lower - (c + shift))) + 1;
+}
+
+int fixChar(const int ch, const int nc) {
+	int shift = nc - ch;
+	int upper = isUpper(ch) ? Z:z;
+	int lower = isUpper(ch) ? A:a;	
+	if (isOutUpper(ch, nc)) {
+		return fixOutUpper(ch, upper, lower, shift);
+	} else if (isOutLower(ch, nc)) {
+		return fixOutLower(ch, upper, lower, shift);
+	}
+	return -1;
+}
+
+int caesar(int c, int shift) {
+	shift %= 26;
+	if (isAlphabet(c)) {	
+		int nc = c + shift;
+		if (isOut(c, nc)) {
+			nc = fixChar(c, nc);
+		}
+		return nc;	
+	}
+	return c;
+}
+
+int main (int argc, char *argv[]) {
+	
+	int numc = atoi(argv[1]);
+	
+	int rot = 1;
+	while (rot <= 26) {
+		printf("%d: ", rot);
+		int counter = 0;
+		while (counter < numc) {
+			putchar(caesar(argv[2][counter], rot));
+			counter++;
+		}
+		printf("\n");
+		rot++;	
+	}
+
+	return 0;
+}

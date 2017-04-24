3abb286c959ca070d8457a27f9e6eab56d521038
Jordan Lewis <jordan_j_lewis@icloud.com>
2017-04-24 16:33:49 +1000

    find holes bug

--- assignment1/numHoles.py
+++ assignment1/numHoles.py
@@ -1,71 +1,41 @@
-def findFirst(pixels, num):
-    for row in xrange(len(pixels)):
-        for col in xrange(len(pixels[0])):
-            if pixels[row][col] == num:
-                return (row, col)
-    return (-1,-1)
-
-def isOnRightWall(pixels, location):
+def isXonRight(pixels, location, x):
     (row, col) = location
-    return col + 1 == len(pixels[0])
+    if x == -1:
+        return col + 1 == len(pixels[0])
+    if col + 1 == len(pixels[0]):
+        return False
+    else:
+        return pixels[row][col + 1] == x
 
-def isOnLeftWall(location):
+def isXonLeft(pixels, location, x):
     (row, col) = location
-    return col == 0
+    if x == -1:
+        return col == 0
+    if col == 0:
+        return False
+    else:
+        return pixels[row][col - 1] == x
 
-def isOnTopWall(pixels, location):
+def isXonTop(pixels, location, x):
     (row, col) = location
-    return row + 1 == len(pixels)
+    if x == -1:
+        return row + 1 == len(pixels)
+    if row + 1 == len(pixels):
+        return False
+    else:
+        return pixels[row + 1][col] == x
 
-def isOnBottomWall(location):
+def isXonBottom(pixels, location, x):
     (row, col) = location
-    return row == 0
+    if x == -1:
+        return row == 0
+    if row == 0:
+        return False
+    else:
+        return pixels[row - 1][col] == x
 
-def isOnWall(pixels, location):
-    return isOnBottomWall(location) or isOnTopWall(pixels, location) or isOnRightWall(pixels, location) or isOnLeftWall(location)
-
-def findNextNum(pixels, current, num):
-    (crow, ccol) = current
-    # look right, up, left, then down
-
-    if not isOnRightWall(pixels, current):
-        if pixels[crow][ccol + 1] == num: # check right
-            return (crow, ccol + 1)
-        if not isOnTopWall(pixels, current): # top right
-            if pixels[crow + 1][ccol + 1] == num:
-                return (crow + 1, ccol + 1)
-
-    if not isOnTopWall(pixels, current):
-        if pixels[crow + 1][ccol] == num: # top
-            return (crow + 1, ccol)
-        if not isOnLeftWall(current):
-            if pixels[crow + 1][ccol - 1] == num: # top left
-                return (crow + 1, ccol - 1)
-
-    if not isOnLeftWall(current):
-        if pixels[crow][ccol - 1] == num: # left
-            return (crow, ccol - 1)
-        if not isOnBottomWall(current): # bottom left
-            if pixels[crow - 1][ccol - 1] == num:
-                return (crow - 1, ccol - 1)
-
-    if not isOnBottomWall(current):
-        if pixels[crow - 1][ccol] == num: # bottom
-            return (crow - 1, ccol)
-        if not isOnRightWall(pixels, current): # bottom right
-            if pixels[crow - 1][ccol + 1] == num:
-                return (crow - 1, ccol + 1)
-
-    return (-1, -1)
-
-def floodFill(pixels, start, fromnum, tonum):
-    (frow, fcol) = start
-    pixels[frow][fcol] = tonum
-    (nrow, ncol) = findNextNum(pixels, (frow, fcol), fromnum)
-    while nrow != -1:
-        pixels[nrow][ncol] = tonum
-        (nrow, ncol) = findNextNum(pixels, (nrow, ncol), fromnum)
-    return pixels
+def isNextTo(pixels, location, x):
+    return isXonBottom(pixels, location, x) or isXonTop(pixels, location, x) or isXonRight(pixels, location, x) or isXonLeft(pixels, location, x)
 
 def printarr(pixels):
     for i in pixels:
@@ -79,23 +49,44 @@ def replaceAll(pixels, num, withNum):
                 pixels[row][col] = withNum
     return pixels
 
-def numberOfHoles(pixels):
-    (frow, fcol) = findFirst(pixels, 0)
-    numTimesFilled = 0
-    # make all 0s to 2s
+def findFirst(pixels, num, after):
+    (arow, acol) = after
+    for row in xrange(len(pixels)):
+        for col in xrange(len(pixels[0])):
+            if pixels[row][col] == num:
+                if row > arow:
+                    return (row, col)
+                elif col > acol:
+                    return (row, col)
+    return (-1,-1)
+
+def aKindaFloodFill(pixels, orig, wall, enc):
+    (frow, fcol) = findFirst(pixels, orig, (0,0))
     while frow != -1:
-        pixels = floodFill(pixels, (frow, fcol), 0, 2)
-        numTimesFilled += 1
-        (frow, fcol) = findFirst(pixels, 0)
-    #0printarr(pixels)
-    # for every 0 that is on a wall, flood fill from it
-    for _ in xrange(numTimesFilled + 1):
-        location = findFirst(pixels, 2)
-        if isOnWall(pixels, location):
-            pixels = floodFill(pixels, location, 2, 1)
-            numTimesFilled -= 1
-    printarr(replaceAll(pixels, 2, 0))
-    return numTimesFilled
+        first = (frow, fcol)
+        setTo = -1
+
+        if isNextTo(pixels, first, -1) or isNextTo(pixels, first, wall):
+            setTo = wall
+        elif isNextTo(pixels, first, 1) or isNextTo(pixels, first, enc):
+            setTo = enc
+
+        pixels[frow][fcol] = setTo
+        if frow + 1 < len(pixels):
+            if isXonTop(pixels, first, orig):
+                pixels[frow + 1][fcol] = setTo
+        if fcol + 1 < len(pixels[0]):
+            if isXonRight(pixels, first, orig):
+                pixels[frow][fcol + 1] = setTo
+
+        (frow, fcol) = findFirst(pixels, orig, first)
+
+def numberOfHoles(pixels):
+    aKindaFloodFill(pixels, 0, 2, 3)
+    printarr(pixels)
+    prev = pixels
+    aKindaFloodFill(pixels, 3, 2, 3)
+    printarr(pixels)
 
 if __name__ == '__main__':
     import openpbm

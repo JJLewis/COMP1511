56cecf200fb60acf1bac1639fd52c9d5e2e6711a
Jordan Lewis <jordan_j_lewis@icloud.com>
2017-04-24 19:18:18 +1000

    create horizontal and vertical balance

---
+++ assignment1/balance.py
@@ -0,0 +1,54 @@
+def horizontalBalance(pixels):
+    height = len(pixels)
+    width = len(pixels[0])
+
+    column_sum = 0
+    n_black_pixels = 0
+
+    v = 0
+    while v < height:
+        h = 0
+        while h < width:
+            if pixels[v][h] == 1:
+                n_black_pixels += 1
+                column_sum += h
+            h += 1
+        v += 1
+
+    return (float(column_sum) / float(n_black_pixels) + 0.5) / float(width)
+
+def verticalBalance(pixels):
+    height = len(pixels)
+    width = len(pixels[0])
+
+    row_sum = 0
+    n_black_pixels = 0
+
+    v = 0
+    while v < height:
+        h = 0
+        while h < width:
+            if pixels[v][h] == 1:
+                n_black_pixels += 1
+                row_sum += v
+            h += 1
+        v += 1
+
+    return (float(row_sum) / float(n_black_pixels) + 0.5) / float(height)
+
+if __name__ == '__main__':
+    import openpbm
+    import getbounding
+    import extractNumber
+
+    fstruct = 'pbms/digit/*N_*F.pbm'
+    for n in xrange(1):
+        print "For number: " + str(n)
+        for f in xrange(1):
+            file = openpbm.fileNameFrom(fstruct, 9, f)
+            pixels = openpbm.read_pbm(file)
+            extracted = extractNumber.extract(pixels, getbounding.getBoundingBox(pixels))
+
+            h = horizontalBalance(extracted)
+            v = verticalBalance(extracted)
+            print "Horizontal: " + str(h) + " Vertical: " + str(v)

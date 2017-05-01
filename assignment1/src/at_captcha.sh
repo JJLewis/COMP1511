e886a14d011e8345abb575a33c1e9b25c4fbd748
Jordan Lewis (UNSW z5165128) <jordan.lewis@student.unsw.edu.au>
2017-05-01 11:23:25 +1000

    Fix auto-test scripts

---
+++ assignment1/src/at_captcha.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+~cs1511/bin/captcha_test --captcha -n 1000 balances.c bounding_box.c copy_pixels.c crack_captcha.c debugger.c feature_array.c guess_functions.c guess_has_hole.c guess_no_holes.c half_image_features.c handy.c hole_count.c manual_guess.c neighbours.c other_metrics.c print_image.c read_pbm.c debugger.h captcha.h  guess.h handy.h neighbours.h split_captcha.c
--- assignment1/src/at_digits.sh
+++ assignment1/src/at_digits.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~cs1511/bin/captcha_test --digit -n 10 balances.c bounding_box.c copy_pixels.c crack_digit.c debugger.c feature_array.c guess_functions.c guess_has_hole.c guess_no_holes.c half_image_features.c handy.c hole_count.c manual_guess.c neighbours.c other_metrics.c print_image.c read_pbm.c tester.c debugger.h captcha.h tester.h guess.h handy.h neighbours.h
+~cs1511/bin/captcha_test --digit -n 1000 balances.c bounding_box.c copy_pixels.c crack_digit.c debugger.c feature_array.c guess_functions.c guess_has_hole.c guess_no_holes.c half_image_features.c handy.c hole_count.c manual_guess.c neighbours.c other_metrics.c print_image.c read_pbm.c debugger.h captcha.h  guess.h handy.h neighbours.h

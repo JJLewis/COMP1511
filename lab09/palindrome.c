fc280ebc88b7025b898656a0cfb5baec5c4aae3d
Jordan Lewis (UNSW z5165128) <jordan.lewis@student.unsw.edu.au>
2017-05-03 11:28:24 +1000

    Lab09 palindrome.c ATP

---
+++ lab09/palindrome.c
@@ -0,0 +1,43 @@
+// Jordan Lewis (jordan.lewis@student.unsw.edu.au)
+// May 02 2017
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFSIZE 4096
+#define TRUE 1
+#define FALSE 0
+
+int lengthOfString(char string[BUFSIZE]) {
+	int count = 0;
+	for (int i = 0; i <= BUFSIZE; i++) {
+		if (string[i] == '\n') {
+			break;
+		}
+		count++;
+	}
+	return count;
+}	
+
+int main (int argc, char* argv[]) {
+	
+	char string[BUFSIZE];
+	printf("Enter a string: ");
+
+	fgets(string, BUFSIZE, stdin);
+	
+	int length = lengthOfString(string);
+	int half = length/2.0;
+	
+	int isPalindrome = TRUE;
+	for (int i = 0; i < half; i++) {
+		if (string[i] != string[length - i - 1]) {
+			isPalindrome = FALSE;
+		}
+	}
+
+	printf("String is %sa palindrome\n", isPalindrome ? "":"not ");
+
+	return 0;
+}

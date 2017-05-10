a9448d3edd40a7b9e6122e25db7d80735bb142da
Jordan Lewis <jordan_j_lewis@icloud.com>
2017-05-10 11:35:58 +1000

    Add debugger files

---
+++ assignment2/debugger.h
@@ -0,0 +1,22 @@
+//
+// Created by Jordan Lewis on 29/4/17.
+//
+
+#ifndef SRC_DEBUGGER_H
+#define SRC_DEBUGGER_H
+
+#endif //SRC_DEBUGGER_H
+
+#define IS_DEBUGGING 0
+
+void print(char *s);
+
+void println();
+
+void printD(double x);
+void printI(int x);
+
+void printarrI(int size, int array[size]);
+void printarrD(int size, double array[size]);
+
+void print2darrI(int height, int width, int array[height][width]);

582876c0050b89b238b59b299e663a68d8547b31
Jordan Lewis <jordan_j_lewis@icloud.com>
2017-05-13 19:31:04 +1000

    Creat empty hand.h/c

---
+++ assignment2/handy.c
@@ -0,0 +1,4 @@
+//
+// Created by Jordan Lewis on 13/5/17.
+//
+
---
+++ assignment2/handy.h
@@ -0,0 +1,8 @@
+//
+// Created by Jordan Lewis on 13/5/17.
+//
+
+#ifndef ASSIGNMENT2_HANDY_H
+#define ASSIGNMENT2_HANDY_H
+
+#endif //ASSIGNMENT2_HANDY_H

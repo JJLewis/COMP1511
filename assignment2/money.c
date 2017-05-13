159d2b80fcd143821b4e84a7d0a662ce41293d07
Jordan Lewis <jordan_j_lewis@icloud.com>
2017-05-13 23:32:27 +1000

    Implement cost_of_travel in money.c

---
+++ assignment2/money.c
@@ -0,0 +1,11 @@
+//
+// Created by Jordan Lewis on 13/5/17.
+//
+
+#include "world.h"
+
+int cost_of_travel(location_t *start, location_t *end) {
+    int distance = true_distance_between(start, end);
+    location_t *petrol = nearest_petrol_station(end, -1);
+    return distance * petrol->price;
+}

127ca9a99fca946be8562567c8a577c0d21f51fd
Jordan Lewis <jordan_j_lewis@icloud.com>
2017-05-13 19:29:37 +1000

    Create jjbot.h, make typedefs for structs

---
+++ assignment2/jjbot.h
@@ -0,0 +1,16 @@
+//
+// Created by Jordan Lewis on 13/5/17.
+//
+
+#ifndef ASSIGNMENT2_JJBOT_H
+#define ASSIGNMENT2_JJBOT_H
+
+#endif //ASSIGNMENT2_JJBOT_H
+
+#include "trader_bot.h"
+
+typedef struct bot bot_t;
+typedef struct location location_t;
+typedef struct cargo cargo_t;
+typedef struct commodity commodity_t;
+typedef struct bot_list bot_list_t;

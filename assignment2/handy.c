4c609a90ce0b1e1187a2ae4ac6b01ae5c262f693
Jordan Lewis <jordan_j_lewis@icloud.com>
2017-05-14 02:34:56 +1000

    Create some handy function
    Mosty double array functions

--- assignment2/handy.c
+++ assignment2/handy.c
@@ -2,3 +2,49 @@
 // Created by Jordan Lewis on 13/5/17.
 //
 
+/*
+ * Finds and return the largest double element in array.
+ */
+double maxElementD(int size, double array[size]) {
+    double largest = array[0];
+    for (int i = 0; i < size; i++) {
+        if (array[i] > largest) {
+            largest = array[i];
+        }
+    }
+    return largest;
+}
+
+/*
+ * Find and returns the index of a given double element in array.
+ */
+int indexOfD(int size, double array[size], double element) {
+    for (int i = 0; i < size; i++) {
+        if (array[i] == element) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+/*
+ * Find the coordinates of the largest element in a matrix
+ */
+double coord_of_max_D(int height, int width, double matrix[height][width], int *row, int *column) {
+    double largest = matrix[0][0];
+    int lRow = 0;
+    int lCol = 0;
+    for (int v = 0; v < height; v++) {
+        for (int h = 0; h < width; h++) {
+            if (matrix[v][h] > largest) {
+                lRow = v;
+                lCol = h;
+                largest = matrix[v][h];
+            }
+        }
+    }
+
+    *row = lRow;
+    *column = lCol;
+    return largest;
+}
--- assignment2/handy.h
+++ assignment2/handy.h
@@ -6,3 +6,7 @@
 #define ASSIGNMENT2_HANDY_H
 
 #endif //ASSIGNMENT2_HANDY_H
+
+double maxElementD(int size, double array[size]);
+int indexOfD(int size, double array[size], double element);
+double coord_of_max_D(int height, int width, double matrix[height][width], int *row, int *column);

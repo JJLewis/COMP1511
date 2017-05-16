fa6ca6ae5b3d300878635bfee06d89d0b7f5981a
Jordan Lewis <jordan_j_lewis@icloud.com>
2017-05-16 23:22:48 +1000

    Start implmenting bot logic, and name bot

---
+++ assignment2/jjbot.c
@@ -0,0 +1,14 @@
+//
+// Created by Jordan Lewis on 16/5/17.
+//
+
+#include "trader_bot.h"
+#include "jjbot.h"
+
+char *get_bot_name(void) {
+    return "Plumpess";
+}
+
+void get_action(struct bot *b, int *action, int *n) {
+    
+}

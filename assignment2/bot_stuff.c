9fe69561c18384257524eb661b0cf9ac67b4df3b
Jordan Lewis <jordan_j_lewis@icloud.com>
2017-05-19 11:21:48 +1000

    Implement simple fuel logic

--- assignment2/bot_stuff.c
+++ assignment2/bot_stuff.c
@@ -3,6 +3,9 @@
 //
 
 #include "jjbot.h"
+#include "world.h"
+#include <math.h>
+#include <stdbool.h>
 
 int max_cargo_amount_for_commodity(bot_t *bot, commodity_t *commodity) {
     int weight_max = bot->maximum_cargo_weight / commodity->weight;
@@ -12,4 +15,22 @@ int max_cargo_amount_for_commodity(bot_t *bot, commodity_t *commodity) {
     } else {
         return volume_max;
     }
+}
+
+/*
+ * Seller and Buyer are the ones being exploited.
+ */
+// TODO: Estimate how many more times a pair will last, if not long, look for the next pair
+int number_of_sustainable_move_turns(bot_t *bot, location_pair_t *pair) {
+    int fuel_left = bot->fuel;
+    int distance = pair->distance;
+    return floorf((double)fuel_left / (double)distance);
+}
+
+// TODO: Should account for distance to the nearest petrol station, and current bot location*
+bool should_refuel(bot_t *bot, location_pair_t *pair) {
+    if (number_of_sustainable_turns(bot, pair) < LOW_FUEL_THRESHOLD) {
+        return true;
+    }
+    return false;
 }
--- assignment2/jjbot.h
+++ assignment2/jjbot.h
@@ -9,14 +9,26 @@
 
 #include "trader_bot.h"
 
+#define LOW_FUEL_THRESHOLD 5
+
 typedef struct bot bot_t;
 typedef struct location location_t;
 typedef struct cargo cargo_t;
 typedef struct commodity commodity_t;
 typedef struct bot_list bot_list_t;
 
+struct location_pair {
+    location_t *seller;
+    location_t *buyer;
+    commodity_t *commodity;
+    int distance;
+};
+
+typedef struct location_pair location_pair_t;
+
 // bot_stuff.c
 int max_cargo_amount_for_commodity(bot_t *bot, commodity_t *commodity);
+int number_of_sustainable_turns(bot_t *bot, location_pair_t *pair);
 
 // money.c
 int cost_of_travel(location_t *start, location_t *end);

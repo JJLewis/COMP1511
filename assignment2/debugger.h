f8f6b516f889883517a6f3b2e337bbd6b395dbcf
Jordan Lewis <jordan_j_lewis@icloud.com>
2017-05-26 19:56:18 +1000

    Lay foundation for debugging

--- assignment2/debugger.h
+++ assignment2/debugger.h
@@ -9,16 +9,16 @@
 
 #define IS_DEBUGGING 1
 
+// debugger.c
 void print(char *s);
-
 void println();
-
 void printD(double x);
 void printI(int x);
-
 void printarrI(int size, int array[size]);
 void printarrD(int size, double array[size]);
-
 void print2darrI(int height, int width, int array[height][width]);
-
 void throw_warning(char *s);
+
+// print_world.c
+void print_location(location_t loc);
+void print_world(struct bot *b);
--- assignment2/print_world.c
+++ assignment2/print_world.c
@@ -5,34 +5,57 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "trader_bot.h"
+#include "structs.h"
+#include "debugger.h"
+
+void print_array_of_locations(location_t locations[], int size) {
+    if (IS_DEBUGGING) {
+        println();
+        for (int i = 0; i < size; i++) {
+            location_t loc = locations[i];
+            print_location(loc);
+            print("");
+        }
+        println();
+    }
+}
+
+void print_location(location_t loc) {
+    if (IS_DEBUGGING) {
+        char output[80] = "";
+        char *name = loc->name;
+        int type = loc->type;
+
+        if (type == LOCATION_START) {
+            sprintf(output, "start");
+        } else if (type == LOCATION_DUMP) {
+            sprintf(output, "dump");
+        } else if (type == LOCATION_OTHER) {
+            sprintf(output, "other");
+        } else if (type == LOCATION_PETROL_STATION) {
+            sprintf(output, "Petrol Station %i units of available fuel for $%i", loc->quantity, loc->price);
+        } else {
+            // Buyer or Seller
+            sprintf(output, "will %s %i units of %s for $%i", type == LOCATION_BUYER ? "buy" : "sell", loc->quantity,
+                    loc->commodity->name, loc->price);
+        }
+
+        printf("%s: %s\n", name, output);
+    }
+}
 
 void print_world(struct bot *b) {
+    if (IS_DEBUGGING) {
+        struct location *head = b->location;
+        struct location *loc = head;
+        bool hasStarted = false;
 
-	struct location *head = b->location;
-	struct location *loc = head;
-	bool hasStarted = false;
-	
-	while (!(loc == head && hasStarted)) {
-		hasStarted = true;
+        while (!(loc == head && hasStarted)) {
+            hasStarted = true;
 
-		char output[80] = "";
-		char *name = loc->name;
-		int type = loc->type;
-		
-		if (type == LOCATION_START) {
-			sprintf(output, "start");
-		} else if (type == LOCATION_DUMP) {
-			sprintf(output, "dump");
-		} else if (type == LOCATION_OTHER) {
-			sprintf(output, "other");
-		} else if (type == LOCATION_PETROL_STATION) {
-			sprintf(output, "Petrol Station %i units of available fuel for $%i", loc->quantity, loc->price);
-		} else {
-			// Buyer or Seller
-			sprintf(output, "will %s %i units of %s for $%i", type == LOCATION_BUYER ? "buy":"sell", loc->quantity, loc->commodity->name, loc->price);
-		}
-		
-		printf("%s: %s\n", name, output);
-		loc = loc->next;
-	}
+            print_location(loc);
+
+            loc = loc->next;
+        }
+    }
 }

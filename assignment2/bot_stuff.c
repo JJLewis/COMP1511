66226884485ae7cc8389ec32c0ecee41020a1468
Jordan Lewis <jordan_j_lewis@icloud.com>
2017-06-03 21:21:06 +1000

    Comment on the prev commit

--- assignment2/bot_stuff.c
+++ assignment2/bot_stuff.c
@@ -87,7 +87,7 @@ bool should_refuel(bot_t bot, action_t action) {
     int target_to_fuel_distance = true_distance_between(target, nearest_petrol);
     int at_target_remaining_fuel = remaining_fuel - distance_to_target;
     int bot_to_fuel_distance = true_distance_between(bot->location, nearest_petrol);
-    if (bot_to_fuel_distance > remaining_fuel) return false;
+    if (bot_to_fuel_distance > remaining_fuel) return false; // Not going to make it to petrol so don't bother, just die
     if (at_target_remaining_fuel < target_to_fuel_distance) return true;
 
     return false;

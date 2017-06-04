629aa0d5ef4866bee54f6f10c42e7385ff18801d
Jordan Lewis <jordan_j_lewis@icloud.com>
2017-06-04 19:36:14 +1000

    Comment througout significant_locations.c

--- assignment2/significant_locations.c
+++ assignment2/significant_locations.c
@@ -8,6 +8,9 @@
 #include "world.h"
 #include "debugger.h"
 
+/*
+ * Return a boolean reflecting whether or not the bot will pass a non-empty petrol station
+ */
 bool will_pass_petrol(location_t start, location_t end) {
     int distance = distance_between(start, end);
     int direction = distance_to_direction(distance);
@@ -26,8 +29,16 @@ bool will_pass_petrol(location_t start, location_t end) {
     return false;
 }
 
-// TODO: Remember that a -1 for min distance will return a 'self sustaining' petrol station, ie, enough fuel to cover the distance travelled for it
-// Should it be 2x the distance?
+/*
+ * Finds the nearest petrol station with a minimum amount of fuel of a specified amount.
+ *
+ * input:
+ *          location: Find the nearest petrol station to this location.
+ *          minimumFuel: The minimum amount of fuel the petrol station must have.
+ *                       Pass -1 for the minimum amount of fuel to be the distance between location and the petrol station.
+ *
+ * returns: The nearest petrol station with at least the minimum specified amount of fuel. If there isn't return NULL.
+ */
 location_t nearest_petrol_station(location_t location, int minimumFuel) {
     location_t stations[MAX_LOCATIONS];
     int numStations = all_petrol_stations(location, stations);
@@ -71,6 +82,16 @@ location_t nearest_petrol_station(location_t location, int minimumFuel) {
     return closest;
 }
 
+/*
+ * Find the closest buyer of a certain commodity to a given location.
+ *
+ * inputs:
+ *          bot: Pointer to the bot struct (used to finding all the buyers of a commodity)
+ *          location: The anchor location, finds the closest buyer to this location.
+ *          commodity: Look for buyers of this commodity.
+ *
+ * returns: The location of the closest buyer of commodity to location.
+ */
 location_t closest_buyer_of_commodity_to(bot_t bot, location_t location, commodity_t commodity) {
     location_t buyers[MAX_LOCATIONS];
     int num_buyers = all_buyers_of_commodity(bot, commodity, buyers);
@@ -89,6 +110,18 @@ location_t closest_buyer_of_commodity_to(bot_t bot, location_t location, commodi
     return closest_buyer;
 }
 
+/*
+ * Find the best buyer of a commodity.
+ * Do this by getting the ratio of price to distance from location.
+ * Enter all the ratios into an array, and pick the highest.
+ *
+ * inputs:
+ *          bot: Pointer to the bot struct (used to finding all the buyers of a commodity)
+ *          location: The anchor location, the distance for the ratio is measured from here.
+ *          commodity: Look for buyers of this commodity.
+ *
+ * returns: The location of the closest buyer of commodity to location.
+ */
 location_t best_buyer_of_commodity_to(bot_t bot, location_t location, commodity_t commodity) {
     location_t buyers[MAX_LOCATIONS];
     int num_buyers = all_buyers_of_commodity(bot, commodity, buyers);
@@ -116,6 +149,16 @@ location_t best_buyer_of_commodity_to(bot_t bot, location_t location, commodity_
     return best_buyer;
 }
 
+/*
+ * Returns a boolean about whether or not the bot will pass a location between start and end.
+ *
+ * inputs:
+ *          start: The start location.
+ *          end: The end location.
+ *          x: The location that the function wants to check if the bot passes.
+ *
+ * returns: True if the bot will pass x, False if not.
+ */
 bool will_pass_location(location_t start, location_t end, location_t x) {
     location_t tracker = start;
     int distance = distance_between(start, end);
@@ -129,6 +172,11 @@ bool will_pass_location(location_t start, location_t end, location_t x) {
     return false;
 }
 
+/*
+ * This function assumes the bot is carrying cargo.
+ * It finds the best closest buyer to the bot of the commodity that the bot is carrying.
+ * Does this by getting the ratio of (price * quantity) to the distance to the buyer and choosing the highest.
+ */
 location_pair_t best_closest_buyer(bot_t bot) {
     commodity_t commodity = bot->cargo->commodity;
     location_t current_location = bot->location;
@@ -162,6 +210,15 @@ location_pair_t best_closest_buyer(bot_t bot) {
     return create_location_pair(current_location, best_buyer);
 }
 
+/*
+ * Returns the location of a petrol station within the shortest route between two locations.
+ *
+ * inputs:
+ *          start: The start location.
+ *          end: The end location.
+ *
+ * returns: A location struct if one is found, otherwise NULL.
+ */
 location_t petrol_between(location_t start, location_t end) {
     location_t petrol_station = NULL;
     location_t tracker = start;
@@ -184,6 +241,18 @@ location_t petrol_between(location_t start, location_t end) {
     return petrol_station;
 }
 
+/*
+ * Finds all buyers of a commodity within the range of fuel left in the bot.
+ *
+ * input:
+ *          bot: The bot, to access how much fuel is left and its current location.
+ *          commodity: Only look for buyers of this commodity.
+ *          buyers[]: This pointer to an array will have the BUYER locations found added to it, from closest to furthest.
+ *
+ * output:
+ *          buyers[]: Array of locations of BUYERS of specified commodity, in ascending order of distance from the bot.
+ *          returns: How many buyers were found in the fuel range.
+ */
 int buyers_in_fuel_range(bot_t bot, commodity_t commodity, location_t buyers[MAX_LOCATIONS]) {
     int fuel = bot->fuel;
     int counter = 0;
@@ -214,16 +283,17 @@ int buyers_in_fuel_range(bot_t bot, commodity_t commodity, location_t buyers[MAX
     return counter;
 }
 
+/*
+ * Finds the best buyer in the fuel range of the bot at a seller.
+ * Does this by finding all buyers of the commodity of the seller the bot is at in the bot's fuel range.
+ * Then makes ratios by calculating the total profit of a trade ((buyer price - seller price) * quantity)
+ * then dividing it by the distance between the seller and the buyer.
+ * It takes the buyer with the highest ratio as the best_buyer.
+ */
 location_t best_buyer_in_range_from_this_seller(bot_t bot) {
     location_t seller = bot->location;
     commodity_t commodity = seller->commodity;
-    /*
-     * Get quantity seller has
-     * get quantity can buy
-     * take the lesser of the two
-     * for each buyer get the lesser of can buy vs can sell
-     * get amount can make from transaction
-     */
+
     int max_loadable = max_cargo_amount_for_commodity(bot, commodity);
     int can_buy_quantity = seller->quantity > max_loadable ? max_loadable : seller->quantity;
 

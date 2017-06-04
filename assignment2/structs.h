b51aca35eada0af7e079aca4ec54e374c3ca6c29
Jordan Lewis <jordan_j_lewis@icloud.com>
2017-06-04 19:38:59 +1000

    Comment throughout structs.c

--- assignment2/structs.h
+++ assignment2/structs.h
@@ -11,19 +11,26 @@ typedef struct cargo *cargo_t;
 typedef struct commodity *commodity_t;
 typedef struct bot_list *bot_list_t;
 
+/*
+ * Holds a pair of locations, for ease of passing as an argument or returning as a function.
+ * Buyer and Seller commodity must/should be the same.
+ */
 struct location_pair {
     location_t seller;
     location_t buyer;
-    commodity_t commodity;
-    int distance;
+    commodity_t commodity; // The commodity of both the buyer and the seller
+    int distance; // True distance between the buyer and the seller
 };
 
 typedef struct location_pair *location_pair_t;
 
+/*
+ * Just wrapping up the get_action values that need to be assigned so that it can be easily used/returned for functions.
+ */
 struct action {
     int action;
     int n;
-    location_t target;
+    location_t target; // Target location struct if the action is MOVE, otherwise NULL
 };
 
 typedef struct action *action_t;

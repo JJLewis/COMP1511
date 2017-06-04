5008427b79c7a5395618a2d64218bfae3fb906a6
Jordan Lewis (UNSW z5165128) <jordan.lewis@student.unsw.edu.au>
2017-06-04 13:15:10 +1000

    Run valgrind and leak test in autotest scripts

--- assignment2/autotest-large.sh
+++ assignment2/autotest-large.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~cs1511/bin/bot_test -w large_world.txt  all_of_type.c bot_stuff.c debugger.c debugger.h handy.c handy.h handy_world.c jjbot.c jjbot.h money.c motion.c significant_locations.c trader_bot.h world.h structs.h print_world.c actions.c
+~cs1511/bin/bot_test -w large_world.txt -v -l all_of_type.c bot_stuff.c debugger.c debugger.h handy.c handy.h handy_world.c jjbot.c jjbot.h money.c motion.c significant_locations.c trader_bot.h world.h structs.h print_world.c actions.c
